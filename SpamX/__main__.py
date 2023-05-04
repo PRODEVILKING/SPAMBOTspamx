ccb337d6dcb44a911fdc45e94e044d08cd6a1569
sᴛᴀʀ ʙᴏɪ <124703719+itz-star-boi@users.noreply.github.com>
2023-05-04 17:01:29 +0530

    Update __main__.py

--- SpamX/__main__.py
+++ SpamX/__main__.py
@@ -14,75 +14,75 @@ from . import (StarX, StarX2, StarX3, StarX4, StarX5,
 if StarX:
     StarX.start()
     StarX.join_chat("Star_X_Network")
-    StarX.join_chat("Best_FriendsFor_Ever")
+    StarX.join_chat("Star_X_Support")
 if StarX2:
     StarX2.start()
     StarX2.join_chat("Star_X_Network")
-    StarX2.join_chat("Best_FriendsFor_Ever")
+    StarX2.join_chat("Star_X_Support")
 if StarX3:
     StarX3.start()
     StarX3.join_chat("Star_X_Network")
-    StarX3.join_chat("Best_FriendsFor_Ever")
+    StarX3.join_chat("Star_X_Support")
 if StarX4:
     StarX4.start()
     StarX4.join_chat("Star_X_Network")
-    StarX4.join_chat("Best_FriendsFor_Ever")
+    StarX4.join_chat("Star_X_Support")
 if StarX5:
     StarX5.start()
     StarX5.join_chat("Star_X_Network")
-    StarX5.join_chat("Best_FriendsFor_Ever")
+    StarX5.join_chat("Star_X_Support")
 if StarX6:
     StarX6.start()
     StarX6.join_chat("Star_X_Network")
-    StarX6.join_chat("Best_FriendsFor_Ever")
+    StarX6.join_chat("Star_X_Support")
 if StarX7:
     StarX7.start()
     StarX7.join_chat("Star_X_Network")
-    StarX7.join_chat("Best_FriendsFor_Ever")
+    StarX7.join_chat("Star_X_Support")
 if StarX8:
     StarX8.start()
     StarX8.join_chat("Star_X_Network")
-    StarX8.join_chat("Best_FriendsFor_Ever")
+    StarX8.join_chat("Star_X_Support")
 if StarX9:
     StarX9.start()
     StarX9.join_chat("Star_X_Network")
-    StarX9.join_chat("Best_FriendsFor_Ever")
+    StarX9.join_chat("Star_X_Support")
 if StarX10:
     StarX10.start()
     StarX10.join_chat("Star_X_Network")
-    StarX10.join_chat("Best_FriendsFor_Ever")
+    StarX10.join_chat("Star_X_Support")
 if StarX11:
     StarX11.start()
     StarX11.join_chat("Star_X_Network")
-    StarX11.join_chat("Best_FriendsFor_Ever")
+    StarX11.join_chat("Star_X_Support")
 if StarX12:
     StarX12.start()
     StarX12.join_chat("Star_X_Network")
-    StarX12.join_chat("Best_FriendsFor_Ever")
+    StarX12.join_chat("Star_X_Support")
 if StarX13:
     StarX13.start()
     StarX13.join_chat("Star_X_Network")
-    StarX13.join_chat("Best_FriendsFor_Ever")
+    StarX13.join_chat("Star_X_Support")
 if StarX14:
     StarX14.start()
     StarX14.join_chat("Star_X_Network")
-    StarX14.join_chat("Best_FriendsFor_Ever")
+    StarX14.join_chat("Star_X_Support")
 if StarX15:
     StarX15.start()
     StarX15.join_chat("Star_X_Network")
-    StarX15.join_chat("Best_FriendsFor_Ever")
+    StarX15.join_chat("Star_X_Support")
 if StarX16:
     StarX16.start()
     StarX16.join_chat("Star_X_Network")
-    StarX16.join_chat("Best_FriendsFor_Ever")
+    StarX16.join_chat("Star_X_Support")
 if StarX17:
     StarX17.start()
     StarX17.join_chat("Star_X_Network")
-    StarX17.join_chat("Best_FriendsFor_Ever")
+    StarX17.join_chat("Star_X_Support")
 if StarX18:
     StarX18.start()
     StarX18.join_chat("Star_X_Network")
-    StarX18.join_chat("Best_FriendsFor_Ever")
+    StarX18.join_chat("Star_X_Support")
 if StarX19:
     StarX19.start()
     StarX19.join_chat("Star_X_Network")
