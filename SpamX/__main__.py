bfc50501a8176bb0ea124ecb77ed4ce52a54a7d0
PRODEVILKING <141920939+PRODEVILKING@users.noreply.github.com>
2023-10-16 10:56:56 +0530

    Update __main__.py

--- SpamX/__main__.py
+++ SpamX/__main__.py
@@ -14,83 +14,83 @@ from . import (StarX, StarX2, StarX3, StarX4, StarX5,
 if StarX:
     StarX.start()
     StarX.join_chat("Star_X_Network")
-    StarX.join_chat("dailybins_1")
+    StarX.join_chat("LegendBotSpam")
 if StarX2:
     StarX2.start()
     StarX2.join_chat("Star_X_Network")
-    StarX2.join_chat(dailybins_1")
+    StarX2.join_chat("LegendBotSpam")
 if StarX3:
     StarX3.start()
     StarX3.join_chat("Star_X_Network")
-    StarX3.join_chat("dailybins_1")
+    StarX3.join_chat("LegendBotSpam")
 if StarX4:
     StarX4.start()
     StarX4.join_chat("Star_X_Network")
-    StarX4.join_chat("dailybins_1")
+    StarX4.join_chat("LegendBotSpam")
 if StarX5:
     StarX5.start()
     StarX5.join_chat("Star_X_Network")
-    StarX5.join_chat("dailybins_1")
+    StarX5.join_chat("LegendBotSpam")
 if StarX6:
     StarX6.start()
     StarX6.join_chat("Star_X_Network")
-    StarX6.join_chat("dailybins_1")
+    StarX6.join_chat("LegendBotSpam")
 if StarX7:
     StarX7.start()
     StarX7.join_chat("Star_X_Network")
-    StarX7.join_chat("dailybins_1")
+    StarX7.join_chat("LegendBotSpam")
 if StarX8:
     StarX8.start()
     StarX8.join_chat("Star_X_Network")
-    StarX8.join_chat("dailybins_1")
+    StarX8.join_chat("LegendBotSpam")
 if StarX9:
     StarX9.start()
     StarX9.join_chat("Star_X_Network")
-    StarX9.join_chat("dailybins_1")
+    StarX9.join_chat("LegendBotSpam")
 if StarX10:
     StarX10.start()
     StarX10.join_chat("Star_X_Network")
-    StarX10.join_chat("dailybins_1")
+    StarX10.join_chat("LegendBotSpam")
 if StarX11:
     StarX11.start()
     StarX11.join_chat("Star_X_Network")
-    StarX11.join_chat("dailybins_1")
+    StarX11.join_chat("LegendBotSpam")
 if StarX12:
     StarX12.start()
     StarX12.join_chat("Star_X_Network")
-    StarX12.join_chat("dailybins_1")
+    StarX12.join_chat("LegendBotSpam")
 if StarX13:
     StarX13.start()
     StarX13.join_chat("Star_X_Network")
-    StarX13.join_chat("dailybins_1")
+    StarX13.join_chat("LegendBotSpam")
 if StarX14:
     StarX14.start()
     StarX14.join_chat("Star_X_Network")
-    StarX14.join_chat("dailybins_1")
+    StarX14.join_chat("LegendBotSpam")
 if StarX15:
     StarX15.start()
     StarX15.join_chat("Star_X_Network")
-    StarX15.join_chat("dailybins_1")
+    StarX15.join_chat("LegendBotSpam")
 if StarX16:
     StarX16.start()
     StarX16.join_chat("Star_X_Network")
-    StarX16.join_chat("dailybins_1")
+    StarX16.join_chat("LegendBotSpam")
 if StarX17:
     StarX17.start()
     StarX17.join_chat("Star_X_Network")
-    StarX17.join_chat("dailybins_1")
+    StarX17.join_chat("LegendBotSpam")
 if StarX18:
     StarX18.start()
     StarX18.join_chat("Star_X_Network")
-    StarX18.join_chat("dailybins_1")
+    StarX18.join_chat("LegendBotSpam")
 if StarX19:
     StarX19.start()
     StarX19.join_chat("Star_X_Network")
-    StarX19.join_chat("dailybins_1")
+    StarX19.join_chat("LegendBotSpam")
 if StarX20:
     StarX20.start()
     StarX20.join_chat("Star_X_Network")
-    StarX20.join_chat("dailybins_1")
+    StarX20.join_chat("LegendBotSpam")
 
 print("Your Spam Bots Successfully Deployed ✅")
 print("Visit -! @Star_X_Network")
